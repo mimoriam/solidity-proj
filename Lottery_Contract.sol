6d89031707e2479ddaca8bdd80c8aae919f58a69
Mimoriam <mimoriam420@gmail.com>
2022-04-12 19:26:31 +0500

    Commit #2 - Initialization

--- Lottery_Contract.sol
+++ Lottery_Contract.sol
@@ -2,10 +2,14 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 contract Lottery {
+    // The number of players that can take part in lottery
     address payable[] public players;
+
+    // The owner of the smart contract
     address public manager;
 
     constructor() {
+        // Set the owner of the contract by the contract compiler
         manager = msg.sender;
     }
 }
